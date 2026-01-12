13c095b8c1d396fe211e255928aaea5a1480345e
Zuzana Miklankova <zmiklank@redhat.com>
2026-01-12 15:01:08 +0100

    fix HKDF
    Signed-off-by: Zuzana Miklankova <zmiklank@redhat.com>

--- vendor/rustls-openssl/build.rs
+++ vendor/rustls-openssl/build.rs
@@ -10,13 +10,11 @@ fn main() {
     println!("cargo:rustc-check-cfg=cfg(ossl320)");
     // Determine whether to work around https://github.com/openssl/openssl/issues/23448
     // according to the OpenSSL version
+    // MODIFIED: Always enable workaround because FIPS provider version may differ from library version
     println!("cargo:rustc-check-cfg=cfg(bugged_add_hkdf_info)");
+    println!("cargo:rustc-cfg=bugged_add_hkdf_info");
     if let Ok(version) = env::var("DEP_OPENSSL_VERSION_NUMBER") {
         let version = u64::from_str_radix(&version, 16).unwrap();
-
-        if (0x3_00_00_00_0..0x3_04_00_00_0).contains(&version) {
-            println!("cargo:rustc-cfg=bugged_add_hkdf_info");
-        }
 
         if version < 0x3_00_00_00_0 {
             println!("cargo:rustc-cfg=fips_module");
